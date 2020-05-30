e26c21b6481f2924dee51666890fced6d534da41
Arihant Jain <arihantjain099@gmail.com>
2020-05-31 04:15:28 +0530

    Update Binary Tree Level Order Traversal.rb

--- Binary Tree Level Order Traversal.rb
+++ Binary Tree Level Order Traversal.rb
@@ -54,3 +54,61 @@ def level_order(root)
     end
     return result
 end
+
+
+
+ # Binary Tree Level Order Traversal II
+
+=begin
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+=end
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[][]}
+
+def level_order_bottom(root)
+    result = []
+    return result if root.nil?
+    queue = Queue.new
+    queue.push root
+    queue.push nil
+    temp = []
+    while(!queue.empty?)
+        node = queue.pop
+        if node.nil?
+            result.push temp
+            temp = []
+            break if queue.empty?
+            queue.push nil  
+        else
+            temp.push node.val
+            queue.push node.left if node.left
+            queue.push node.right if node.right
+        end
+    end
+    return result.reverse
+end
+
