402be362f3276d395eb6f2c137c9d5e8b6036c45
Arihant Jain <arihantjain099@gmail.com>
2020-05-31 02:40:52 +0530

    Create Maximum Depth of Binary Tree.rb

---
+++ Maximum Depth of Binary Tree.rb
@@ -0,0 +1,40 @@
+=begin
+Maximum Depth of Binary Tree
+
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+
+=end
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+
+def max_depth(root)
+    return 0 if root.nil?
+    # max depth is max height in (left tree,right tree) + 1 (root height) 
+    return 1 + [max_depth(root.left), max_depth(root.right)].max
+end
+
